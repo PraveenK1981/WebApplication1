266a7dd9bba31271c04178e30fa8e755d83c7bcb
praveen_srivastava <praveen_srivastava@BCH-653098>
2024-03-07 17:13:57 +0530

    Add project files.

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            
+              //DataSet  ds = SqlHelper.ExecuteDataset(SqlHelper.ConnectionString, CommandType.Text, "select  top 10 * from [RDCMaster]");
+             
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
